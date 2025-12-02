6741a8caca3af28b85f22ebd9c197d07c6c16992
Joshikii <CDCJDN00S30C351V@studium.unict.it>
2025-12-02 20:03:26 +0100

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   // IMPORTANTE: Se usi GitHub Pages con sottocartella
-  site: 'https://joshikii.github.io',
+  site: 'https://www.animefocus.me',
   base: "",
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
 });
